4bd0780fc814c41edc49a070ecda3c018ca0f67a
Gonzalo Diaz <gonzalo@olapic.com>
2018-01-19 14:40:31 -0300

    Agregar soporte para import masivo y export automatico

--- js/app.js
+++ js/app.js
@@ -53,3 +53,128 @@ angular.module('BRApp', [])
         $scope.reset();
         $scope.br = BancoRio.br;
     }])
+
+    .controller('BRAutogenerateController', ['$scope', 'BancoRio', function($scope, BancoRio) {
+        $scope.br = BancoRio.br;
+
+        $scope.makeDescription = function(textLength) {
+          var text = "";
+          var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+          for (var i = 0; i < textLength; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+          return text;
+        }
+
+        $scope.masiveImport = function(fileContent) {
+          var allLines = fileContent.split(/\r\n|\n/);
+          var headers = allLines[0].split(',');
+          for (var i = 1; i < allLines.length; i++) {
+            // split content based on comma
+            var data = allLines[i].split(',');
+            if (data.length == headers.length) {
+              var row = [];
+              for (var j = 0; j < headers.length; j++) {
+                row.push(data[j]);
+              }
+              // Estructura del archivo
+              // importe,es_santander,cbu,cuit,tipo_cuenta,sucursal,numero_de_cuenta,concepto
+              transfer = null;
+              descripcion =
+                is_santander = row[1] === 'SI' ? true : false;
+              if (is_santander) {
+                transfer = new SantanderOtrasCuentas();
+                transfer.sucursal_destino = row[5];
+                transfer.numero_cuenta_destino = row[6];
+                transfer.tipo_cuenta_destino = row[4];
+                transfer.importe = row[0];
+                transfer.descripcion = $scope.makeDescription(20);
+                transfer.codigo_concepto_transferencia = row[7];
+              } else {
+                transfer = new OtrosBancos();
+                transfer.importe = row[0];
+                transfer.descripcion = $scope.makeDescription(20);
+                transfer.cbu = row[2],
+                  transfer.codigo_concepto_transferencia = row[7];
+                transfer.cuit = row[3],
+                  transfer.tipo_cuenta_destino = row[4];
+              }
+              $scope.br.addRow(angular.copy(transfer));
+            }
+          }
+        };
+
+        $scope.$watch("fileContent", function(newValue, oldValue) {
+          if ($scope.fileContent != null) {
+            $scope.masiveImport($scope.fileContent);
+          }
+        });
+
+        $scope.saveTextAsFile = function(data, filename) {
+          if (!data) {
+            console.error('Console.save: No data')
+            return;
+          }
+
+          if (!filename) filename = 'console.json'
+
+          var blob = new Blob([data], {
+              type: 'text/plain'
+            }),
+            e = document.createEvent('MouseEvents'),
+            a = document.createElement('a')
+
+          // FOR IE:
+          if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+            window.navigator.msSaveOrOpenBlob(blob, filename);
+          } else {
+            var e = document.createEvent('MouseEvents'),
+              a = document.createElement('a');
+
+            a.download = filename;
+            a.href = window.URL.createObjectURL(blob);
+            a.dataset.downloadurl = ['text/plain', a.download, a.href].join(':');
+            e.initEvent('click', true, false, window,
+              0, 0, 0, 0, 0, false, false, false, false, 0, null);
+            a.dispatchEvent(e);
+          }
+        };
+
+        $scope.walk = function(arr, n, fn) {
+          for (var i = 0; i < arr.length; i += n)
+            fn(arr.slice(i, i + n));
+        };
+
+        $scope.exportFile = function() {
+          var fileName = "transferencias.txt";
+          var content = $scope.br.dump();
+          $scope.saveTextAsFile(content, fileName);
+        };
+      }])
+
+
+      .directive('fileReader', function() {
+        return {
+          scope: {
+            fileReader: "="
+          },
+          link: function(scope, element) {
+            $(element).on('change', function(changeEvent) {
+              var files = changeEvent.target.files;
+              if (files.length) {
+                var r = new FileReader();
+                r.onload = function(e) {
+                  var contents = e.target.result;
+                  scope.$apply(function() {
+                    scope.fileReader = contents;
+                    scope.testing = contents;
+                  });
+                };
+
+                r.readAsText(files[0]);
+              }
+            });
+          }
+        };
+      });
